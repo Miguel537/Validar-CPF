349349e990b5ca72405146a689051068122aa23f
Miguel Joaquim <migueljoaquim409@gmail.com>
2025-05-22 21:03:30 -0300

    Feitas algumas alterações no HTML e o JS para que a menssagem de CPF válido pudesse notificar

--- cpf.js
+++ cpf.js
@@ -1,63 +1,37 @@
-function validarCPF(cpf){
-    cpf = cpf.replace(/[^\d]+/g,"");
+function validarCPF(cpf) {
+  cpf = cpf.replace(/[^\d]+/g, '');
 
-    if(cpf.lenght !== 11 || /^(\d)\1+$/.test(cpf)){
-        return false;
-    }
+  if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
 
-    let soma = 0;
-    let resto; 
-    
-    //validar o 1 DV
-    for (let i = 1; i <= 9; i ++){
-        soma += parseInt(cpf.substring(i-1, i) * (11-i));
-    }
+  let soma = 0;
+  for (let i = 0; i < 9; i++) {
+    soma += parseInt(cpf.charAt(i)) * (10 - i);
+  }
+  let resto = (soma * 10) % 11;
+  if (resto === 10 || resto === 11) resto = 0;
+  if (resto !== parseInt(cpf.charAt(9))) return false;
 
-    resto = (soma * 10) % 11;
-    if(resto === 10 || resto === 11){
-        resto = 0;
-    }
+  soma = 0;
+  for (let i = 0; i < 10; i++) {
+    soma += parseInt(cpf.charAt(i)) * (11 - i);
+  }
+  resto = (soma * 10) % 11;
+  if (resto === 10 || resto === 11) resto = 0;
+  if (resto !== parseInt(cpf.charAt(10))) return false;
 
-    if(resto !== parseInt(cpf.substring (9, 10))){
-        return false;
-    }
-
-
-    //Validando o 2 digito
-    soma = 0;
-    for (let i = 2; i <=10; i ++){
-        soma += parseInt(cpf.substring(i-1, i) * (12 - i));
-    }
-
-    resto = (soma * 10) % 11;
-    if(resto === 10 || resto === 11){
-        resto = 0;
-    }
-
-    if(resto !== parseInt(cpf.substring (10, 11))){
-        return false;
-    }
-
-    return true; 
+  return true;
 }
 
-document.getElementById('cpfForm').addEventListener("submit", function(e){
-    e.preventDefault();
+document.getElementById("cpfForm").addEventListener("submit", function(e){
+  e.preventDefault();
+  const cpf = document.getElementById("cpf").value;
+  const messageDiv = document.getElementById("message");
 
-    const cpfInput = document. 
-    getElementById('cpf').value;
-    const messageDiv = document.getElementById ('message');
-
-    if(validarCPF(cpfInput)){
-        messageDiv.textContent = "CPF Válido";
-        messageDiv.className = "massage sucess";
-
-    } 
-    
-    else{
-        messageDiv.textContent = "CPF Inválido";
-        messageDiv.className = "message erro";
-    }
-
-    messageDiv.style.display = "block";
+  if (validarCPF(cpf)) {
+    messageDiv.textContent = "CPF válido!";
+    messageDiv.style.color = "green";
+  } else {
+    messageDiv.textContent = "CPF inválido!";
+    messageDiv.style.color = "red";
+  }
 });
